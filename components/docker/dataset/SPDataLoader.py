c8fac823e88c67da031acaecf3d8b0e9cc7eff19
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-13 09:12:43 +0800

    change dc to app

--- components/docker/dataset/SPDataLoader.py
+++ components/docker/dataset/SPDataLoader.py
@@ -2,15 +2,15 @@
 from __future__ import absolute_import, print_function
 
 import torch
-from suanpan.docker import DockerComponent as dc
-from suanpan.docker.arguments import Int, Bool
+from suanpan.app import app
+from suanpan.app.arguments import Int, Bool
 from arguments import PytorchDataloader, PytorchDataset
 
 
-@dc.input(PytorchDataset(key="inputData"))
-@dc.param(Int(key="batchSize", default=100))
-@dc.param(Bool(key="shuffle", default=True))
-@dc.output(PytorchDataloader(key="outputData"))
+@app.input(PytorchDataset(key="inputData"))
+@app.param(Int(key="batchSize", default=100))
+@app.param(Bool(key="shuffle", default=True))
+@app.output(PytorchDataloader(key="outputData"))
 def SPDataLoader(context):
     # 从 Context 中获取相关数据
     args = context.args

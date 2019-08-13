3a45ad54243c9ffa5b95c9eedbe88a3fd5d39949
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-13 09:13:45 +0800

    change dc to app

--- components/docker/dataset/SPImageFolder.py
+++ components/docker/dataset/SPImageFolder.py
@@ -1,20 +1,19 @@
 # coding=utf-8
 from __future__ import absolute_import, print_function
 
-from suanpan.docker import DockerComponent as dc
-# from suanpan.docker.arguments import Folder
+from suanpan.app import app
+
 from utils.folder import ImageFolder
 from arguments import PytorchDataset, PytorchTransModel, FolderPath
 
 
-@dc.input(FolderPath(key="inputData"))
-@dc.input(PytorchTransModel(key="inputModel"))
-@dc.output(PytorchDataset(key="outputData"))
+@app.input(FolderPath(key="inputData"))
+@app.input(PytorchTransModel(key="inputModel"))
+@app.output(PytorchDataset(key="outputData"))
 def SPImageFolder(context):
     # 从 Context 中获取相关数据
     args = context.args
-    
-    print(args.inputData)
+
     dataset = ImageFolder(oss_root=args.inputData, transform=args.inputModel)
 
     return dataset

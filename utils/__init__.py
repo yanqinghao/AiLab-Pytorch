c1ae5332fc53b0e7bbe0578b213971a5be78e369
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-13 09:18:33 +0800

    add plot methods

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,16 +1,20 @@
 # coding=utf-8
 from __future__ import absolute_import, print_function
+import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import hiddenlayer as hl
+from graphviz import Digraph
+from suanpan.screenshots import screenshots
+from suanpan.utils import image
 from .mnist import MNIST
 from .folder import ImageFolder
 
 
-def hello():
-    print("Utils Hello!")
-
-
 def getLayerName(moduleList, match):
     layersName = []
-    for name, module in moduleList.named_children():
+    for name, module in moduleList:
         if match in name:
             layersName.append(int(name.replace(match + "_", "")))
     return (
@@ -18,3 +22,96 @@ def getLayerName(moduleList, match):
         if layersName
         else "{}_{}".format(match, 0)
     )
+
+
+def transImgSave(dataset, transform):
+    folder = "/out_data/"
+    pathtmp = ""
+    for img, _, paths in dataset:
+        if isinstance(paths, str):
+            save_path = os.path.join(folder, paths)
+        else:
+            paths_img = "{}.png".format(paths)
+            save_path = os.path.join(folder, paths_img)
+        transformed_img = transform(img)
+        if not os.path.exists(os.path.split(save_path)[0]):
+            os.makedirs(os.path.split(save_path)[0])
+        transformed_img.save(save_path)
+        if not pathtmp:
+            pathtmp = paths
+    screenshots.save(np.asarray(transformed_img))
+    if isinstance(pathtmp, str):
+        pathlist = pathtmp.split("/")
+        output = os.path.join(folder, *pathlist[:6])
+    else:
+        output = "/out_data"
+    return output
+
+
+def trainingLog(log):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    epochs = log["epoch"]
+    acc = log["train_acc"]
+    val_acc = log["val_acc"]
+
+    ax.plot(epochs, acc, "b", label="Training acc")
+    ax.plot(epochs, val_acc, "r", label="Validation acc")
+    ax.text(0, 7, "Training acc", fontsize=10, color="b")
+    ax.text(0, 6.5, "Validation acc", fontsize=10, color="r")
+    fig.canvas.draw()
+
+    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+    plt.close(fig)
+
+    return screenshots.save(data)
+
+
+def plotLayers(model, input_size=None):
+    file_name = "screenshots"
+    output_size = (
+        calOutput(model)
+        if model.layers
+        else [1] + [model.input_size[2]] + list(model.input_size[:2])
+        if len(model.input_size) == 3
+        else [1] + list(model.input_size)
+    )
+    name_list = [i[0] for i in model.layers]
+    model_name = "{} Layer".format(name_list[-1]) if name_list else "Input Layer"
+    input_name = (
+        "IN (N {})".format("".join(["* {}".format(i) for i in input_size[1:]]))
+        if model.layers
+        else None
+    )
+    output_name = "OUT (N {})".format(
+        "".join(["* {}".format(i) for i in output_size[1:]])
+    )
+    dot = Digraph(comment="Pytorch Model")
+    dot.attr("graph", size="12,12", dpi="300", bgcolor="#E8E8E8")
+    if input_name:
+        dot.node(input_name)
+    dot.node(model_name, shape="box", style="filled", color="lightblue2")
+    dot.node(output_name)
+    if input_name:
+        dot.edge(input_name, model_name)
+    dot.edge(model_name, output_name)
+    dot.render(file_name, format="png")
+    layer_image = image.read("{}.png".format(file_name))
+    screenshots.save(layer_image)
+
+
+def calOutput(model):
+    input_size = (
+        [1] + [model.input_size[-1]] + list(model.input_size[:-1])
+        if len(model.input_size) == 3
+        else [1] + list(model.input_size)
+    )
+    return model(torch.zeros(input_size)).shape if model.layers else input_size
+
+
+def datasetScreenshot(dataset, screenshot):
+    for img, _, _ in dataset:
+        screenshot.save(np.asarray(img))
+        break

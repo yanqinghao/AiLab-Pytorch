7666be1ad3de72b7d3b77e214390bcf462382ca0
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-14 09:41:35 +0800

    fix conv2d arguments

--- components/docker/layers/SPConv2D.py
+++ components/docker/layers/SPConv2D.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, print_function
 
 import torch.nn as nn
-from suanpan.app.arguments import Int, String
+from suanpan.app.arguments import Int, String, Bool
 from app import app
 from arguments import PytorchLayersModel
 from utils import getLayerName, plotLayers, calOutput
@@ -15,6 +15,9 @@ from utils import getLayerName, plotLayers, calOutput
 @app.param(Int(key="stride", default=1))
 @app.param(Int(key="padding", default=2))
 @app.param(String(key="paddingMode", default="zeros"))
+@app.param(Int(key="dilation", default=1))
+@app.param(Int(key="groups", default=1))
+@app.param(Bool(key="bias", default=True))
 @app.output(PytorchLayersModel(key="outputModel"))
 def SPConv2D(context):
     # 从 Context 中获取相关数据
@@ -33,6 +36,9 @@ def SPConv2D(context):
             stride=args.stride,
             padding=args.padding,
             padding_mode=args.paddingMode,
+            dilation=args.dilation,
+            groups=args.groups,
+            bias=args.bias,
         ),
     )
     model.layers.append((name, getattr(model, name)))

f6b1677d5cf9c94532f6a2d5bf5a1fe7cc8cae51
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-14 09:42:21 +0800

    maxpool2d model

--- components/docker/layers/SPMaxPool2D.py
+++ components/docker/layers/SPMaxPool2D.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function
 
 import torch.nn as nn
 
-from suanpan.app.arguments import Int
+from suanpan.app.arguments import Int, Bool
 from app import app
 from arguments import PytorchLayersModel
 from utils import getLayerName, plotLayers, calOutput
@@ -12,6 +12,10 @@ from utils import getLayerName, plotLayers, calOutput
 @app.input(PytorchLayersModel(key="inputModel"))
 @app.param(Int(key="kernelSize", default=2))
 @app.param(Int(key="stride", default=2))
+@app.param(Int(key="padding", default=0))
+@app.param(Int(key="dilation", default=1))
+@app.param(Bool(key="returnIndices", default=False))
+@app.param(Bool(key="ceilMode", default=False))
 @app.output(PytorchLayersModel(key="outputModel"))
 def SPMaxPool2D(context):
     # 从 Context 中获取相关数据
@@ -20,7 +24,18 @@ def SPMaxPool2D(context):
     model = args.inputModel
     inputSize = calOutput(model)
     name = getLayerName(model.layers, "MaxPool2D")
-    setattr(model, name, nn.MaxPool2d(kernel_size=args.kernelSize, stride=args.stride))
+    setattr(
+        model,
+        name,
+        nn.MaxPool2d(
+            kernel_size=args.kernelSize,
+            stride=args.stride,
+            padding=args.padding,
+            dilation=args.dilation,
+            return_indices=args.returnIndices,
+            ceil_mode=args.ceilMode,
+        ),
+    )
     model.layers.append((name, getattr(model, name)))
     plotLayers(model, inputSize)
 

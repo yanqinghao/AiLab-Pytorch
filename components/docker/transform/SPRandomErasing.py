d1556a05d0961e415fddd85e96df03807941dc43
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-21 15:04:20 +0800

    random erasing(torch 1.2)

---
+++ components/docker/transform/SPRandomErasing.py
@@ -0,0 +1,62 @@
+"""
+Created on Sun Aug 19 2019
+@author: Yan Qinghao
+transforms
+"""
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+import torchvision.transforms as transforms
+
+from suanpan.app.arguments import Int, Bool, String, Folder, Float, ListOfFloat
+from app import app
+from arguments import PytorchTransModel, PytorchDataset
+from utils import transImgSave, mkFolder
+
+
+@app.input(PytorchDataset(key="inputData"))
+@app.param(
+    Float(
+        key="p",
+        default=0.5,
+        help="probability that the random erasing operation will be performed.",
+    )
+)
+@app.param(
+    ListOfFloat(
+        key="scale",
+        default=[0.02, 0.33],
+        help="range of proportion of erased area against input image.",
+    )
+)
+@app.param(
+    ListOfFloat(
+        key="ratio", default=[0.3, 3.3], help="range of aspect ratio of erased area."
+    )
+)
+@app.param(Int(key="value", default=0, help="erasing value. "))
+@app.param(
+    Bool(key="inplace", default=False, help="boolean to make this transform inplace.")
+)
+@app.output(PytorchTransModel(key="outputModel"))
+@app.output(Folder(key="outputData"))
+def SPRandomErasing(context):
+    """
+    Crop the given PIL Image at a random location.
+    """
+    args = context.args
+
+    transform = transforms.RandomErasing(
+        p=args.p,
+        scale=(*args.scale,),
+        ratio=(*args.ratio,),
+        value=args.value,
+        inplace=args.inplace,
+    )
+    folder = transImgSave(args.inputData, transform) if args.inputData else mkFolder()
+
+    return transform, folder
+
+
+if __name__ == "__main__":
+    SPRandomErasing()

d90258b6d84c4633d66bee9ac93068533c172ed4
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-21 15:07:59 +0800

    tensor to image

---
+++ components/docker/transform/SPToPILImage.py
@@ -0,0 +1,38 @@
+"""
+Created on Sun Aug 19 2019
+@author: Yan Qinghao
+transforms
+"""
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+import torchvision.transforms as transforms
+
+from suanpan.app import app
+from suanpan.app.arguments import Folder, String
+from arguments import PytorchTransModel
+
+
+@app.input(Folder(key="inputData"))
+@app.param(
+    String(
+        key="mode",
+        default=None,
+        help="color space and pixel depth of input data (optional)."
+             "1, L, P, RGB, RGBA, CMYK, YCbCr, LAB, HSV, I, F",
+    )
+)
+@app.output(PytorchTransModel(key="outputModel"))
+def SPToPILImage(context):
+    """
+    Convert a tensor or an ndarray to PIL Image.
+    """
+    args = context.args
+
+    transform = transforms.ToPILImage(mode=args.mode)
+
+    return transform
+
+
+if __name__ == "__main__":
+    SPToPILImage()

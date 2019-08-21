661369e31e7c41cc362c6c03245baa267f923552
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-21 15:02:34 +0800

    random affine

---
+++ components/docker/transform/SPRandomAffine.py
@@ -0,0 +1,66 @@
+"""
+Created on Sun Aug 19 2019
+@author: Yan Qinghao
+transforms
+"""
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+import torchvision.transforms as transforms
+
+from suanpan.app.arguments import Float, Folder, ListOfFloat, Int
+from app import app
+from arguments import PytorchTransModel, PytorchDataset
+from utils import transImgSave, mkFolder
+
+
+@app.input(PytorchDataset(key="inputData"))
+@app.param(Float(key="degrees", default=0, help=" Range of degrees to select from."))
+@app.param(
+    ListOfFloat(
+        key="translate",
+        default=None,
+        help="tuple of maximum absolute fraction for horizontal and vertical translations.",
+    )
+)
+@app.param(ListOfFloat(key="scale", default=None, help="scaling factor interval"))
+@app.param(Float(key="shear", default=None, help="Range of degrees to select from."))
+@app.param(
+    Int(
+        key="resample",
+        default=0,
+        help="An optional resampling filter.PIL.Image.BILINEAR 2, PIL.Image.NEAREST 0,"
+        " PIL.Image.BICUBIC 3",
+    )
+)
+@app.param(
+    Int(
+        key="fillcolor",
+        default=0,
+        help="Optional fill color (Tuple for RGB Image And int for grayscale) for the"
+        " area outside the transform in the output image.",
+    )
+)
+@app.output(PytorchTransModel(key="outputModel"))
+@app.output(Folder(key="outputData"))
+def SPRandomAffine(context):
+    """
+    Random affine transformation of the image keeping center invariant
+    """
+
+    args = context.args
+    transform = transforms.RandomAffine(
+        args.degrees,
+        translate=(*args.translate, ),
+        scale=(*args.scale, ),
+        shear=args.shear,
+        resample=args.resample,
+        fillcolor=args.fillcolor,
+    )
+    folder = transImgSave(args.inputData, transform) if args.inputData else mkFolder()
+
+    return transform, folder
+
+
+if __name__ == "__main__":
+    SPRandomAffine()

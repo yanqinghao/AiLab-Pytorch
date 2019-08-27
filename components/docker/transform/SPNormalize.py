06bead42c85be486b7f3d29490424d0db1006205
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-27 10:57:33 +0800

    del visual

--- components/docker/transform/SPNormalize.py
+++ components/docker/transform/SPNormalize.py
@@ -3,10 +3,9 @@ from __future__ import absolute_import, print_function
 
 import torchvision.transforms as transforms
 
-from suanpan.app.arguments import Bool, Folder, ListOfFloat
+from suanpan.app.arguments import Bool, ListOfFloat
 from app import app
 from arguments import PytorchTransModel, PytorchDataset
-from utils import transImgSave, mkFolder
 
 
 @app.input(PytorchDataset(key="inputData"))
@@ -28,7 +27,6 @@ from utils import transImgSave, mkFolder
     Bool(key="inplace", default=False, help="Bool to make this operation in-place.")
 )
 @app.output(PytorchTransModel(key="outputModel"))
-@app.output(Folder(key="outputData"))
 def SPNormalize(context):
     """
     Normalize a tensor image with mean and standard deviation. 
@@ -36,9 +34,8 @@ def SPNormalize(context):
 
     args = context.args
     transform = transforms.Normalize(args.mean, args.std, inplace=args.inplace)
-    folder = transImgSave(args.inputData, transform) if args.inputData else mkFolder()
 
-    return transform, folder
+    return transform
 
 
 if __name__ == "__main__":

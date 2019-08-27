f96cb47a199a7470bad6e171318c8fdb2c8b3d67
YanQinghao <woshiyanqinghao@gmail.com>
2019-08-27 10:57:09 +0800

    visual

--- components/docker/transform/SPCompose.py
+++ components/docker/transform/SPCompose.py
@@ -3,16 +3,21 @@ from __future__ import absolute_import, print_function
 
 import torchvision.transforms as transforms
 
+from suanpan.app.arguments import Folder
+
 from suanpan.app import app
-from arguments import PytorchTransModel
+from arguments import PytorchTransModel, PytorchDataset
+from utils import transImgSave, mkFolder
 
 
+@app.input(PytorchDataset(key="inputData"))
 @app.input(PytorchTransModel(key="inputModel1"))
 @app.input(PytorchTransModel(key="inputModel2"))
 @app.input(PytorchTransModel(key="inputModel3"))
 @app.input(PytorchTransModel(key="inputModel4"))
 @app.input(PytorchTransModel(key="inputModel5"))
 @app.output(PytorchTransModel(key="outputModel"))
+@app.output(Folder(key="outputData"))
 def SPCompose(context):
     # 从 Context 中获取相关数据
     args = context.args
@@ -22,8 +27,11 @@ def SPCompose(context):
         if transform:
             transformLst.append(transform)
     transformsAug = transforms.Compose(transformLst)
+    folder = (
+        transImgSave(args.inputData, transformsAug) if args.inputData else mkFolder()
+    )
 
-    return transformsAug
+    return transformsAug, folder
 
 
 if __name__ == "__main__":

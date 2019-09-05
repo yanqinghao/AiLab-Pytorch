5078295016f3fa8270751bef9e1e0d0d2e8b7e13
YanQinghao <woshiyanqinghao@gmail.com>
2019-09-05 14:27:09 +0800

    build

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-set -e
+docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker-gpu:$1 -f docker/docker_yanqing_gpu/Dockerfile .
+sed -i "s/cpu/cu100/g" .dockerignore
+docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker:$1 -f docker/docker_yanqing/Dockerfile .
+sed -i "s/cu100/cpu/g" .dockerignore
 
-VERSION=1.0.0
-
-. docker/build.sh $VERSION $@
+docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker:$1
+docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker-gpu:$1

235fe8128fd36f70d0cdf0a867286371f85c9068
YanQinghao <woshiyanqinghao@gmail.com>
2019-09-05 14:41:59 +0800

    fix

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker-gpu:$1 -f docker/docker_yanqing_gpu/Dockerfile .
 sed -i "s/cpu/cu100/g" .dockerignore
 docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker:$1 -f docker/docker_yanqing/Dockerfile .

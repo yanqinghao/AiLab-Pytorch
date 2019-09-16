d932fc602dca164a1a9f8164416ca852b5207acd
YanQinghao <woshiyanqinghao@gmail.com>
2019-09-16 15:25:51 +0800

    fine tuning

---
+++ components/docker/SPFineTuning.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.arguments import ListOfString
+from suanpan.log import logger
+from app import app
+from arguments import PytorchLayersModel, PytorchFinetuningModel
+
+
+@app.input(PytorchFinetuningModel(key="inputModel1"))
+@app.input(PytorchLayersModel(key="inputModel2"))
+@app.param(ListOfString(key="fineTuning", default=None))
+@app.output(PytorchLayersModel(key="outputModel"))
+def SPFineTuning(context):
+    # 从 Context 中获取相关数据
+    args = context.args
+    # 查看上一节点发送的 args.inputData 数据
+    model = args.inputModel2
+    pretrainedFineTuning = args.inputModel1
+    if args.fineTuning:
+        for name, param in model.named_parameters():
+            if ".".join(name.split(".")[:-1]) in args.fineTuning:
+                logger.info("{} layer unfreezed.".format(name))
+                param.requires_grad = True
+    if pretrainedFineTuning["value"]:
+        for name, param in model.named_parameters():
+            if ".".join(name.split(".")[1:-1]) in pretrainedFineTuning["value"]:
+                logger.info("{} layer unfreezed.".format(name))
+                param.requires_grad = True
+    return model
+
+
+if __name__ == "__main__":
+    SPFineTuning()

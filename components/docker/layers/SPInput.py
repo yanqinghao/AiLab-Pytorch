7f1e9e491567599150c9a65867b297e899e8ab72
YanQinghao <woshiyanqinghao@gmail.com>
2019-11-14 18:00:03 +0800

    type params

--- components/docker/layers/SPInput.py
+++ components/docker/layers/SPInput.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import, print_function
 
-from suanpan.app.arguments import Folder, ListOfInt
+from suanpan.app.arguments import Folder, ListOfInt, String
 from suanpan.log import logger
 from app import app
 from arguments import PytorchLayersModel, SPNet
@@ -9,14 +9,15 @@ from utils import plotLayers, getScreenshotPath
 
 
 @app.input(Folder(key="inputData"))
-@app.param(ListOfInt(key="inputSize", default=[28, 28, 1]))
+@app.param(String(key="type", default="image", help="image, text"))
+@app.param(ListOfInt(key="inputSize"))
 @app.output(PytorchLayersModel(key="outputModel"))
 def SPInput(context):
     args = context.args
+    model = SPNet(args.inputSize, args.type)
+    model.layers["Input"] = (None, getScreenshotPath())
     if len(args.inputSize) > 3:
         logger.error("Wrong image size, pls input like (28,28,1).")
-    model = SPNet(args.inputSize)
-    model.layers["Input Layer"] = (None, getScreenshotPath())
     plotLayers(model)
     return model
 

12b3aca58b49ad540de7f46e6dea361855261fb0
YanQinghao <woshiyanqinghao@gmail.com>
2019-11-21 22:33:41 +0800

    math op

--- arguments.py
+++ arguments.py
@@ -29,6 +29,35 @@ class SPNet(nn.Module):
         return out
 
 
+class SPMathOP(nn.Module):
+    def __init__(self, input_size, task, model_list, op, param=None):
+        super(SPMathOP, self).__init__()
+        self.layers = {}
+        self.class_to_idx = None
+        self.input_size = tuple(input_size) if input_size else None
+        self.task = task
+        self.model_list = model_list
+        self.op = op
+        self.param = param
+
+    def forward(self, x, offsets=None):
+        out = x
+        mid = []
+        for model in self.model_list:
+            mid.append(model(out))
+        if self.op == "add":
+            out = getattr(torch, self.op)(*mid)
+        elif self.op == "cat":
+            out = getattr(torch, self.op)((*mid,), **self.param)
+        for _, j in self.layers.items():
+            if j[0] is not None:
+                if isinstance(j[0], nn.EmbeddingBag):
+                    out = j[0](out, offsets)
+                else:
+                    out = j[0](out)
+        return out
+
+
 class PytorchLayersModel(Model):
     FILETYPE = "layers"
 

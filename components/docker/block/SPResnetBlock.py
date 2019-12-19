1aadd1eb6c08400ea026101a1090130f6ebe7124
YanQinghao <woshiyanqinghao@gmail.com>
2019-12-19 22:34:18 +0800

    resnet block

---
+++ components/docker/block/SPResnetBlock.py
@@ -0,0 +1,83 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+import torch.nn as nn
+from suanpan.app.arguments import Int
+from app import app
+from arguments import PytorchLayersModel
+from utils import getLayerName, plotLayers, calOutput, getScreenshotPath
+
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        groups=groups,
+        bias=False,
+        dilation=dilation,
+    )
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class ResnetBlock(nn.Module):
+    def __init__(self, planes, stride=1, groups=1, base_width=64, dilation=1):
+        super(ResnetBlock, self).__init__()
+        if groups != 1 or base_width != 64:
+            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(planes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        downsample = None
+        if stride != 1 or self.inplanes != planes:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes, stride), nn.BatchNorm2d(planes)
+            )
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+@app.input(PytorchLayersModel(key="inputModel"))
+@app.param(Int(key="planes", default=64))
+@app.output(PytorchLayersModel(key="outputModel"))
+def SPResnetBlock(context):
+    # 从 Context 中获取相关数据
+    args = context.args
+    # 查看上一节点发送的 args.inputData 数据
+    model = args.inputModel
+    inputSize = calOutput(model)
+    name = getLayerName(model.layers, "ResnetBlock")
+    setattr(model, name, ResnetBlock(args.planes))
+    model.layers[name] = (getattr(model, name), getScreenshotPath())
+    plotLayers(model, inputSize)
+
+    return model
+
+
+if __name__ == "__main__":
+    SPResnetBlock()

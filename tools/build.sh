b90408ac3b3a5994997bb464651c202d2333add0
YanQinghao <woshiyanqinghao@gmail.com>
2019-12-19 11:53:02 +0800

    namespace

--- tools/build.sh
+++ tools/build.sh
@@ -1,11 +1,15 @@
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker-gpu:$1 -f docker/docker_yanqing_gpu/Dockerfile .
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-stream-gpu:$1 -f docker/stream_yanqing_gpu/Dockerfile .
-sed -i "s/cpu/cu100/g" .dockerignore
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker:$1 -f docker/docker_yanqing/Dockerfile .
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-stream:$1 -f docker/stream_yanqing/Dockerfile .
-sed -i "s/cu100/cpu/g" .dockerignore
+NAMESPACE=(shuzhi,shuzhi-amd64)
+for i in ${NAMESPACE}
+do
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker-gpu:$1 -f docker/docker_yanqing_gpu/Dockerfile .
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-stream-gpu:$1 -f docker/stream_yanqing_gpu/Dockerfile .
+    sed -i "s/cpu/cu100/g" .dockerignore
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker:$1 -f docker/docker_yanqing/Dockerfile .
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-stream:$1 -f docker/stream_yanqing/Dockerfile .
+    sed -i "s/cu100/cpu/g" .dockerignore
 
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-stream:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker-gpu:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-stream-gpu:$1
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker:$1
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-stream:$1
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker-gpu:$1
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-stream-gpu:$1
+done

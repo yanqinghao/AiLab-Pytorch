e1a76c5a3d2cc6cbad7377695cd95ca8e913f22d
YanQinghao <woshiyanqinghao@gmail.com>
2020-01-07 14:18:55 +0800

    docker build args

--- tools/build.sh
+++ tools/build.sh
@@ -1,11 +1,11 @@
 NAMESPACE=("shuzhi-amd64")
 for i in ${NAMESPACE[*]}
 do
-    docker build -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker-gpu:$1 -f docker/docker_yanqing_gpu/Dockerfile .
-    docker build -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-stream-gpu:$1 -f docker/stream_yanqing_gpu/Dockerfile .
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker-gpu:$1 -f docker/docker_yanqing_gpu/Dockerfile .
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-stream-gpu:$1 -f docker/stream_yanqing_gpu/Dockerfile .
     sed -i "s/cpu/cu100/g" .dockerignore
-    docker build -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker:$1 -f docker/docker_yanqing/Dockerfile .
-    docker build -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-stream:$1 -f docker/stream_yanqing/Dockerfile .
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker:$1 -f docker/docker_yanqing/Dockerfile .
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-stream:$1 -f docker/stream_yanqing/Dockerfile .
     sed -i "s/cu100/cpu/g" .dockerignore
 
     docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker:$1

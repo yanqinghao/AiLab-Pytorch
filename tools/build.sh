7a289b4b015688e73c03589fc4274f2fd75f6453
YanQinghao <woshiyanqinghao@gmail.com>
2020-10-15 16:25:21 +0800

    build docker

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,37 @@
-NAMESPACE=("shuzhi-amd64")
-for i in ${NAMESPACE[*]}
-do
-    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker-gpu:$1 -f docker/docker_yanqing_gpu/Dockerfile .
-    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker:$1 -f docker/docker_yanqing/Dockerfile .
+#!/bin/bash
 
-    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker:$1
-    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/pytorch-docker-gpu:$1
+set -e
+
+VERSION=$(bash tools/version.sh)
+DOCKERBASE=("suanpan-python-sdk", "suanpan-python-sdk-cuda")
+if [[ $1 == "master" ]]; then
+    echo "build from suanpan-python-sdk:3.7 and suanpan-python-sdk-cuda:3.7"
+    echo "build pytorch-docker:latest and ${VERSION} and pytorch-docker-gpu:latest and ${VERSION}"
+    TAGS=("3.7")
+    BUILD_VERSIONS=(${VERSION})
+    BUILD_TAGS=("latest")
+else
+    echo "build from suanpan-python-sdk:preview-3.7 and suanpan-python-sdk-cuda:preview-3.7"
+    echo "build pytorch-docker:preview and preview-${VERSION} and pytorch-docker-gpu:preview and preview-${VERSION}"
+    TAGS=("preview-3.7")
+    BUILD_VERSIONS=("preview-${VERSION}")
+    BUILD_TAGS=("preview")
+fi
+BUILDNAMES=("pytorch-docker", "pytorch-docker-gpu")
+REQUIREMENTS=("requirements.txt", "requirements.txt")
+ENTRYPOINT=("/sbin/my_init", "/usr/bin/dumb-init")
+NAMESPACE="shuzhi-amd64"
+for ((i = 0; i < ${#TAGS[@]}; i++)); do
+    for ((j = 0; j < ${#BUILDNAMES[@]}; j++)); do
+        docker build --build-arg NAME_SPACE=${NAMESPACE} --build-arg DOCKER_BASE=${DOCKERBASE[j]} \
+            --build-arg PYTHON_VERSION=${TAGS[i]} --build-arg REQUIREMENTS_FILE=${REQUIREMENTS[j]} \
+            --build-arg ENTRY_POINT=${ENTRYPOINT[j]} -t \
+            registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[j]}:${BUILD_VERSIONS[i]} \
+            -f docker/Dockerfile .
+        docker push registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[j]}:${BUILD_VERSIONS[i]}
+
+        docker tag registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[j]}:${BUILD_VERSIONS[i]} \
+            registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[j]}:${BUILD_TAGS[i]}
+        docker push registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[j]}:${BUILD_TAGS[i]}
+    done
 done

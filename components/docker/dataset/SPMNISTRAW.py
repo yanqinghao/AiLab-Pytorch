a13e55fee53905fd30bf28d4b35af7072819c9a9
YanQinghao <woshiyanqinghao@gmail.com>
2020-10-16 13:19:08 +0800

    fix output

--- components/docker/dataset/SPMNISTRAW.py
+++ components/docker/dataset/SPMNISTRAW.py
@@ -14,9 +14,9 @@ def SPMNISTRAW(context):
     storage = StorageProxy(None, None)
     storage.setBackend(type=args.storageType)
 
-    storage.download("common/data/mnist", args.outputDir)
+    storage.download("common/data/mnist", "/tmp/mnist")
 
-    return args.outputDir
+    return "/tmp/mnist"
 
 
 if __name__ == "__main__":

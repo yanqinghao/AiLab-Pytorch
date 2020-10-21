5a0b5b0e6e1063ea61aabbd2edcfe00d7058e4e1
YanQinghao <woshiyanqinghao@gmail.com>
2020-10-21 11:29:18 +0800

    fix io

--- utils/io.py
+++ utils/io.py
@@ -2,11 +2,9 @@ import torch
 
 
 def load_model(file_path):
-    with open(file_path, "rb") as f:
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        return torch.load(f, map_location=device)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    return torch.load(file_path, map_location=device)
 
 
 def dump_model(value, file_path):
-    with open(file_path, "wb") as f:
-        torch.save(value, f)
+    torch.save(value, file_path)
